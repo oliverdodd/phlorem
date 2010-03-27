4e88b350db42972a89c79b1971b3c942896a07e2
Oliver Dodd <oliver.dodd@gmail.com>
2010-03-27 19:10:53 -0400

    verbiage tweak

--- LoremIpsum.php
+++ LoremIpsum.php
@@ -5,7 +5,7 @@
  */
 class LoremIpsum {
 
-	/* The Lorem Ipsum Common Paragraph */
+	/* The Lorem Ipsum Standard Paragraph */
 	protected $standard = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	private $li_words = array("a", "ac", "accumsan", "ad", "adipiscing",
 			"aenean", "aliquam", "aliquet", "amet", "ante", "aptent", "arcu",
